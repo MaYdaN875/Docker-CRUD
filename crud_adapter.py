105d32d9b9c24c6a91a93bfb2c73025e75e70d2e
MaYdaN875 <danielleon0100@gmail.com>
2025-05-21 23:06:42 -0600

    modified adapter to implement singleton to admin adapter

--- crud_adapter.py
+++ crud_adapter.py
@@ -7,6 +7,13 @@ class CRUDInterface:
 
 
 class CRUDAdminAdapter(CRUDInterface):
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(CRUDAdminAdapter, cls).__new__(cls)
+        return cls._instance
+
     def ejecutar_menu(self, puerto):
         iniciar_menu_crud_completo(puerto)
 
